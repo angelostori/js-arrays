2517eaa57eaa4a39775280904865cf0b771b2053
angelostori <angelostori13@gmail.com>
2025-10-08 17:08:50 +0200

    fix the ex. number 2

--- 01-arrays/script.js
+++ 01-arrays/script.js
@@ -22,15 +22,16 @@ console.log(fourthTeacher);
 
 
 // 2. Sostituisci il quinto insegnante nell'array teachers con 'Patrick'
-
+/*------------------------BONUS------------------------------------------------*/
 //tramite il metodo splice applicato alla lista prendo l'elemento nella posizione 4
 //specifico la quantità di elementi da sostituire, in questo caso 1 e specifico il
 //nuovo valore 
-teachers.splice(4 , 1 , 'Patrick')
+//teachers.splice(4 , 1 , 'Patrick')
 //eseguo una verifica
+//console.log(teachers.at(4));
+/*------------------------------------------------------------------------------*/
+teachers[4] = 'Patrick'
 console.log(teachers.at(4));
-
-
 
 
 // 3. Rimuovi l'ultimo insegnante dall'array e salvalo nella variabile lastTeacher qui sotto
