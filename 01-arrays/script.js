cffa62eb74b036607e2ba178480e091e3a1ec0c7
angelostori <angelostori13@gmail.com>
2025-10-08 17:05:26 +0200

    find an index by its value

--- 01-arrays/script.js
+++ 01-arrays/script.js
@@ -72,7 +72,10 @@ console.log(teachers);
 
 // 7. Trova l'indice dell'insegnante 'Lewis' nell'array teachers
 // e salvalo nella variabile lewisIndex
-const lewisIndex = null;
+const lewisIndex = teachers.indexOf('Lewis');
+
+
+
 
 // 8. Verifica se l'array teachers è vuoto
 // e salva il risultato nella variabile isTeachersEmpty
