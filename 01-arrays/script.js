c47a7aa5a01940f0e4ddf4a2879ceb8ed67e0653
angelostori <angelostori13@gmail.com>
2025-10-08 16:57:30 +0200

    add an element in the first position of the list

--- 01-arrays/script.js
+++ 01-arrays/script.js
@@ -63,6 +63,12 @@ console.log(teachers);
 
 
 // 6. Aggiungi un insegnante di nome 'Sarah' all'inizio dell'array teachers
+teachers.unshift('Sarah')
+//verifico che effettivamente sia in coda alla lista
+console.log(teachers);
+
+
+
 
 // 7. Trova l'indice dell'insegnante 'Lewis' nell'array teachers
 // e salvalo nella variabile lewisIndex
